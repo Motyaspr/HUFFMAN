c003d7f2b491d7632ed791022c84684cb1f0dd4f
Матвей Сприкут <sprikutm@gmail.com>
2018-06-02 12:46:47 +0300

    fix cmake

---
+++ cmake-build-release/CMakeFiles/huflib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huflib.dir/lib/decoder.cpp.o"
+  "CMakeFiles/huflib.dir/lib/builder.cpp.o"
+  "CMakeFiles/huflib.dir/lib/tree.cpp.o"
+  "CMakeFiles/huflib.dir/lib/compressor.cpp.o"
+  "libhuflib.pdb"
+  "libhuflib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/huflib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
